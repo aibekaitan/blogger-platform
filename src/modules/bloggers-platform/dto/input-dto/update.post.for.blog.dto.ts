dc40874f77b58a9ac33275f926cfd5056458c5cc
Aibek <aibekaitan1@gmail.com>
2026-03-10 02:46:55 +0500

    hw17 commit - 10

--- src/modules/bloggers-platform/dto/input-dto/update.post.for.blog.dto.ts
+++ src/modules/bloggers-platform/dto/input-dto/update.post.for.blog.dto.ts
@@ -1,15 +1,22 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class UpdatePostForBlogDto {
+  @Transform(({ value }) => (value ?? '').trim())
   @IsString()
   @IsNotEmpty()
+  @Length(1, 30)
   title: string;
 
+  @Transform(({ value }) => (value ?? '').trim())
   @IsString()
   @IsNotEmpty()
+  @Length(1, 100)
   shortDescription: string;
 
+  @Transform(({ value }) => (value ?? '').trim())
   @IsString()
   @IsNotEmpty()
+  @Length(1, 1000)
   content: string;
 }
